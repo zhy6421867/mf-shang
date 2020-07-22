029f66d4c7aaead6fa174c468e4ebbd0f4a5e644
钟永豪 <zhy6421867@qq.com>
2020-07-23 01:03:22 +0800

    update register create

--- common/application.js
+++ common/application.js
@@ -1,5 +1,5 @@
 export default {
-	host: 'https://unidemo.dcloud.net.cn/',
+	host: 'http://shang.mcfeetest.com/',
 	// host: 'http://www.utdex.net/',
 	imageHost: 'http://www.utdex.net/',
 	// imageHost: 'http://47.112.111.19:8082/', 
